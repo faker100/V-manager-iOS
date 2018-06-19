6ae43f65fe01113bd24a3c0284dbadc7834ee7ea
ZhaoMirror <zhaomirror@mac-mini.local>
2018-06-19 18:13:21 +0800

    合并版本

--- V-Manager/V-Manager/我的/MineViewController.m
+++ V-Manager/V-Manager/我的/MineViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "MineViewController.h"
+#import "InputPhoneNoViewController.h"
+#import "AccountTableViewController.h"
 
-@interface MineViewController ()
+@interface MineViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *headPortraitImageV;
 
 @end
 
@@ -17,11 +20,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title=@"我的";
+    [self initUI];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+//  方法：设置头像样式
+-(void)initUI{
+    
+    
+    /**
+     *  添加手势：也就是当用户点击头像了之后，对这个操作进行反应
+     */
+    //允许用户交互
+    _headPortraitImageV.userInteractionEnabled = YES;
+    
+    //初始化一个手势
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc]initWithTarget:self
+                                                                               action:@selector(alterHeadPortrait:)];
+    
+    //给imageView添加手势
+    [_headPortraitImageV addGestureRecognizer:singleTap];
+}
+
+//  方法：alterHeadPortrait
+-(void)alterHeadPortrait:(UITapGestureRecognizer *)gesture{
+    /**
+     *  弹出提示框
+     */
+    //初始化提示框
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    //按钮：从相册选择，类型：UIAlertActionStyleDefault
+    [alert addAction:[UIAlertAction actionWithTitle:@"从相册选择" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        //初始化UIImagePickerController
+        UIImagePickerController *PickerImage = [[UIImagePickerController alloc]init];
+        //获取方式1：通过相册（呈现全部相册），UIImagePickerControllerSourceTypePhotoLibrary
+        //获取方式2，通过相机，UIImagePickerControllerSourceTypeCamera
+        //获取方法3，通过相册（呈现全部图片），UIImagePickerControllerSourceTypeSavedPhotosAlbum
+        PickerImage.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        //允许编辑，即放大裁剪
+        PickerImage.allowsEditing = YES;
+        //自代理
+        PickerImage.delegate = self;
+        //页面跳转
+        [self presentViewController:PickerImage animated:YES completion:nil];
+    }]];
+    //按钮：拍照，类型：UIAlertActionStyleDefault
+    [alert addAction:[UIAlertAction actionWithTitle:@"拍照" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action){
+        /**
+         其实和从相册选择一样，只是获取方式不同，前面是通过相册，而现在，我们要通过相机的方式
+         */
+        UIImagePickerController *PickerImage = [[UIImagePickerController alloc]init];
+        //获取方式:通过相机
+        PickerImage.sourceType = UIImagePickerControllerSourceTypeCamera;
+        PickerImage.allowsEditing = YES;
+        PickerImage.delegate = self;
+        [self presentViewController:PickerImage animated:YES completion:nil];
+    }]];
+    //按钮：取消，类型：UIAlertActionStyleCancel
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+//PickerImage完成后的代理方法
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    //定义一个newPhoto，用来存放我们选择的图片。
+    UIImage *newPhoto = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+    _headPortraitImageV.image = newPhoto;
+    //将头像保存到本地
+    [self.userinfo setHeadPortrait:newPhoto name:@"headPortrait"];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,7 +115,14 @@
     
    
 }
-
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 1 && indexPath.section==1) {
+        //调用
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"mineStoryboard" bundle:nil];
+        AccountTableViewController * vc = (AccountTableViewController *)[storyboard  instantiateViewControllerWithIdentifier:@"sid_accountconfig"];
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+}
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
